d1cebac696d07cdb1baa1026c1d9371c515e7ee9
Karel Nagel <nagelkarel@gmail.com>
2023-04-17 12:04:42 +0300

    mask decoder separated

---
+++ sam-rs/src/modeling/mask_decoder/mlp.rs
@@ -0,0 +1,61 @@
+// 100% GPT-4 code
+
+use tch::{nn, Tensor};
+
+#[derive(Debug)]
+pub struct MLP {
+    layers: nn::Sequential,
+    num_layers: usize,
+    sigmoid_output: bool,
+}
+
+impl MLP {
+    pub fn new(
+        vs: &nn::Path,
+        input_dim: i64,
+        hidden_dim: i64,
+        output_dim: i64,
+        num_layers: usize,
+        sigmoid_output: bool,
+    ) -> Self {
+        let mut layers = nn::seq();
+        let mut last_dim = input_dim;
+        for i in 0..num_layers {
+            let next_dim = if i == num_layers - 1 {
+                output_dim
+            } else {
+                hidden_dim
+            };
+            layers = layers.add(nn::linear(
+                vs / format!("layer_{}", i),
+                last_dim,
+                next_dim,
+                Default::default(),
+            ));
+            last_dim = next_dim;
+        }
+        Self {
+            layers,
+            num_layers,
+            sigmoid_output,
+        }
+    }
+}
+impl nn::Module for MLP {
+    fn forward(&self, x: &Tensor) -> Tensor {
+        let intermediate_outputs = self.layers.forward_all(x, None);
+        let mut x = intermediate_outputs[0].shallow_clone();
+        for i in 1..self.num_layers {
+            x = if i < self.num_layers - 1 {
+                x.relu()
+            } else {
+                intermediate_outputs[i].shallow_clone()
+            };
+        }
+        if self.sigmoid_output {
+            x.sigmoid()
+        } else {
+            x
+        }
+    }
+}
--- sam-rs/src/modeling/mask_decoder/mod.rs
+++ sam-rs/src/modeling/mask_decoder/mod.rs
@@ -1,3 +1,5 @@
+mod mlp;
+use mlp::MLP;
 use tch::{
     nn::{self, Embedding, Module, Sequential},
     Tensor,
@@ -169,65 +171,5 @@ impl MaskDecoder {
 
         let iou_pred = self.iou_prediction_head.forward(&iou_token_out);
         return (masks, iou_pred);
-    }
-}
-
-// 100% GPT-4 code
-
-#[derive(Debug)]
-struct MLP {
-    layers: nn::Sequential,
-    num_layers: usize,
-    sigmoid_output: bool,
-}
-
-impl MLP {
-    fn new(
-        vs: &nn::Path,
-        input_dim: i64,
-        hidden_dim: i64,
-        output_dim: i64,
-        num_layers: usize,
-        sigmoid_output: bool,
-    ) -> Self {
-        let mut layers = nn::seq();
-        let mut last_dim = input_dim;
-        for i in 0..num_layers {
-            let next_dim = if i == num_layers - 1 {
-                output_dim
-            } else {
-                hidden_dim
-            };
-            layers = layers.add(nn::linear(
-                vs / format!("layer_{}", i),
-                last_dim,
-                next_dim,
-                Default::default(),
-            ));
-            last_dim = next_dim;
-        }
-        Self {
-            layers,
-            num_layers,
-            sigmoid_output,
-        }
-    }
-}
-impl nn::Module for MLP {
-    fn forward(&self, x: &Tensor) -> Tensor {
-        let intermediate_outputs = self.layers.forward_all(x, None);
-        let mut x = intermediate_outputs[0].shallow_clone();
-        for i in 1..self.num_layers {
-            x = if i < self.num_layers - 1 {
-                x.relu()
-            } else {
-                intermediate_outputs[i].shallow_clone()
-            };
-        }
-        if self.sigmoid_output {
-            x.sigmoid()
-        } else {
-            x
-        }
     }
 }
