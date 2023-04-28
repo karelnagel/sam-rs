b584799922bd9d148fa47cd5750f3503cfbeccb2
Karel Nagel <nagelkarel@gmail.com>
2023-04-28 13:12:11 +0300

    attention ok

--- sam-rs/src/modeling/transformer/attention.rs
+++ sam-rs/src/modeling/transformer/attention.rs
@@ -8,8 +8,6 @@ use burn::{
 //     after projection to queries, keys, and values.
 #[derive(Debug, Module)]
 pub struct Attention<B: Backend> {
-    _embedding_dim: usize,
-    _internal_dim: usize,
     num_heads: usize,
     q_proj: Linear<B>,
     k_proj: Linear<B>,
@@ -26,13 +24,11 @@ impl<B: Backend> Attention<B> {
         let v_proj = LinearConfig::new(embedding_dim, internal_dim).init();
         let out_proj = LinearConfig::new(internal_dim, embedding_dim).init();
         Self {
-            _embedding_dim: embedding_dim,
-            _internal_dim: internal_dim,
             num_heads,
-            q_proj: q_proj.into(),
-            k_proj: k_proj.into(),
-            v_proj: v_proj.into(),
-            out_proj: out_proj.into(),
+            q_proj,
+            k_proj,
+            v_proj,
+            out_proj,
         }
     }
 
@@ -40,15 +36,13 @@ impl<B: Backend> Attention<B> {
         let shape = x.dims();
         let (b, n, c) = (shape[0], shape[1], shape[2]);
         let x = x.reshape([b, n, num_heads, c / num_heads]);
-        // x.transpose(1, 2)
-        x.transpose()
+        x.swap_dims(1, 2)
     }
 
     fn _recombine_heads(&self, x: Tensor<B, 4>) -> Tensor<B, 3> {
         let shape = x.dims();
         let (b, n_heads, n_tokens, c_per_head) = (shape[0], shape[1], shape[2], shape[3]);
-        x.transpose()
-            // x.transpose(1, 2)
+        x.swap_dims(1, 2)
             .reshape([b, n_tokens, n_heads * c_per_head])
     }
 
@@ -65,10 +59,10 @@ impl<B: Backend> Attention<B> {
 
         // # Attention
         let c_per_head = q.dims()[3];
-        let mut attn = q.matmul(k.transpose());
+        let mut attn = q.matmul(k.permute([0, 1, 3, 2]));
         // let mut attn = q.matmul(&k.transpose(2, 3)); // B x N_heads x N_tokens x N_tokens
         attn = attn / (c_per_head as f64).sqrt();
-        attn = softmax(attn, usize::MAX);
+        attn = softmax(attn, 3);
 
         // # Get output
         let out = attn.matmul(v);
@@ -79,26 +73,19 @@ impl<B: Backend> Attention<B> {
 
 #[cfg(test)]
 mod test {
-    use crate::tests::helpers::{random_tensor, Test, TestBackend};
+    use crate::tests::helpers::{load_module, random_tensor, Test, TestBackend};
 
     #[test]
     fn test_attention() {
         let mut attention = super::Attention::<TestBackend>::new(32, 8, Some(1));
-        let file = Test::open("transformer_attention");
-        file.compare("embedding_dim", attention._embedding_dim);
-        file.compare("internal_dim", attention._internal_dim);
-        file.compare("num_heads", attention.num_heads);
-        // file.compare("q_proj_size", attention.q_proj.ws.size());
-        // file.compare("k_proj_size", attention.k_proj.ws.size());
-        // file.compare("v_proj_size", attention.v_proj.ws.size());
-        // file.compare("out_proj_size", attention.out_proj.ws.size());
+        // attention = load_module("transformer_attention", attention);
 
         // Forward
         let q = random_tensor([1, 32, 32], 1);
         let k = random_tensor([1, 32, 32], 2);
         let v = random_tensor([1, 32, 32], 3);
         let output = attention.forward(q.clone(), k.clone(), v.clone());
-        let file = Test::open("transformer_attention_forward");
+        let file = Test::open("transformer_attention");
         file.compare("q", q);
         file.compare("k", k);
         file.compare("v", v);
